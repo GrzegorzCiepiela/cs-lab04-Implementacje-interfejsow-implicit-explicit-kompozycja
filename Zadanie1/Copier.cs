a5eeb3044a76b78e392d9ce1c79fba744e098c0c
GrzegorzCiepiela <101497515+GrzegorzCiepiela@users.noreply.github.com>
2022-05-06 22:47:27 +0200

    Add files via upload

---
+++ Zadanie1/Copier.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ver1;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; set; }
+        public int ScanCounter { get; set; }
+        public new int Counter { get; set; }
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.on)
+            {
+                PrintCounter++;
+                Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Print: {document.GetFileName()}");
+            }
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.PDF)
+        {
+            document = null;
+
+            if (GetState() == IDevice.State.on)
+            {
+                switch (formatType)
+                {
+                    case IDocument.FormatType.PDF:
+                        document = new PDFDocument($"PDFScan{ScanCounter}.pdf");
+                        break;
+
+                    case IDocument.FormatType.JPG:
+                        document = new ImageDocument($"ImageScan{ScanCounter}.jpg");
+                        break;
+
+                    case IDocument.FormatType.TXT:
+                        document = new TextDocument($"TextScan{ScanCounter}.txt");
+                        break;
+                }
+
+                Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Scan: {document.GetFileName()}");
+                ScanCounter++;
+
+            }
+        }
+        public void ScanAndPrint()
+        {
+            if (GetState() == IDevice.State.on)
+            {
+                Scan(out IDocument document);
+                Print(in document);
+            }
+        }
+
+        public new void PowerOn()
+        {
+            if (GetState() == IDevice.State.off)
+            {
+                Counter++;
+                base.PowerOn();
+            }
+        }
+    }
+}
+
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ver1;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+
+            xerox.ScanAndPrint();
+            System.Console.Write("Liczba uruchomień kserokopiarki: ");
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.Write("Liczba wydrukowanych dokumentow: ");
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.Write("Liczba zeskanowanych dokumentow: ");
+            System.Console.WriteLine(xerox.ScanCounter);
+        }
+    }
+}
