8d25e7e05765f4612b75931b026e64bd631cf99c
GrzegorzCiepiela <101497515+GrzegorzCiepiela@users.noreply.github.com>
2022-05-07 12:34:23 +0200

    Add files via upload

---
+++ Zadanie2/MultiFunctionalDevice.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ver1;
+
+namespace Zadanie2
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; set; }
+        public int ScanCounter { get; set; }
+        public new int Counter { get; set; }
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.on)
+            {
+                PrintCounter++;
+                Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Print: {document.GetFileName()}");
+            }
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.PDF)
+        {
+            document = null;
+
+            if (GetState() == IDevice.State.on)
+            {
+                switch (formatType)
+                {
+                    case IDocument.FormatType.PDF:
+                        document = new PDFDocument($"PDFScan{ScanCounter}.pdf");
+                        break;
+
+                    case IDocument.FormatType.JPG:
+                        document = new ImageDocument($"ImageScan{ScanCounter}.jpg");
+                        break;
+
+                    case IDocument.FormatType.TXT:
+                        document = new TextDocument($"TextScan{ScanCounter}.txt");
+                        break;
+                }
+
+                Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Scan: {document.GetFileName()}");
+                ScanCounter++;
+
+            }
+        }
+
+        public void ScanAndPrint()
+        {
+            if (GetState() == IDevice.State.on)
+            {
+                Scan(out IDocument document);
+                Print(in document);
+            }
+        }
+
+        public new void PowerOn()
+        {
+            if (GetState() == IDevice.State.off)
+            {
+                Counter++;
+                base.PowerOn();
+            }
+        }
+    }
+    public class MultiFunctionalDevice : Copier, IFax
+    {
+        public int FaxCounter { get; set; }
+
+        public void Fax(in IDocument document)
+        {
+            if (GetState() == IDevice.State.on)
+            {
+                FaxCounter++;
+                Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Fax: {document.GetFileName()}");
+            }
+        }
+        public void ScanAndPrintAndFax(string v)
+        {
+            if (GetState() == IDevice.State.on)
+            {
+                Scan(out IDocument document);
+                Print(in document);
+                Fax(in document);
+            }
+        }
+
+        public void Send(IDocument document, string faxNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ver1;
+using Zadanie1;
+using Zadanie2;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xerox = new MultiFunctionalDevice();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+
+            xerox.ScanAndPrint();
+            System.Console.Write("Liczba uruchomień kserokopiarki: ");
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.Write("Liczba wydrukowanych dokumentow: ");
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.Write("Liczba zeskanowanych dokumentow: ");
+            System.Console.WriteLine(xerox.ScanCounter);
+
+
+            System.Console.WriteLine(" \n");
+            
+
+            xerox.ScanAndPrintAndFax("fax receiver");
+            System.Console.Write("Liczba uruchomień kserokopiarki: ");
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.Write("Liczba wydrukowanych dokumentow: ");
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.Write("Liczba zeskanowanych dokumentow: ");
+            System.Console.WriteLine(xerox.ScanCounter);
+            System.Console.Write("Liczba wyslanych Fax: ");
+            System.Console.WriteLine(xerox.FaxCounter);
+        }
+    }
+}
